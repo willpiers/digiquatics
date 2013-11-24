c2ed585900aa6299d90e20f61a7f12c29e1104ff
Josh Duffy <jduffy@mines.edu>
2013-11-23 19:53:40 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AquaticsApp::Application.config.secret_key_base = '872f81b8afe3a40ab8ea486d996629a5a968883f15439fdd30c3aa22a20fa0dceb748c34a466ebc514fd839c9270dfebeeb70fd5130d63bab21c080189edb863'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AquaticsApp::Application.config.session_store :cookie_store, key: '_aquatics_app_session'
