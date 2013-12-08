9e6950211cb6ca4d927f47297b5e74d7d9876cd2
Michael Pierce <michael@affektive.com>
2013-12-08 10:23:18 -0700

    Sign-Up Screen

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -18,4 +18,37 @@ describe "User pages" do
 	  it { should have_content('Create New User') }
 	  it { should have_title('Create New User') }
 	end
+
+describe "signup" do
+
+    before { visit signup_path }
+
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before dof
+        fill_in "First Name",   with: "First"
+        fill_in "Last Name",    with: "Last"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+        fill_in "Suit Size",   with: "M"
+        fill_in "Shirt Size",   with: "M"
+        fill_in "Sex",   with: "M"
+        fill_in "Date of Birth",   with: "2000-01-30"
+        fill_in "Date of Hire",   with: "2013-09-13"
+        fill_in "Phone Number",   with: "7203879691"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
 end
