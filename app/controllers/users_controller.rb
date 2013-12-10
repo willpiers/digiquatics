30c7f428eccc96a0259894667672d4833299b844
Michael Pierce <michael@affektive.com>
2013-12-09 22:09:28 -0700

    Added user edit and user index (Partial)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
+  def index
+    @users = User.all
+  end
   
 	def show
 		@user = User.find(params[:id])
@@ -7,6 +13,18 @@ class UsersController < ApplicationController
   	def new
   		@user = User.new
   	end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
   def create
     @user = User.new(user_params)
@@ -18,8 +36,23 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-def user_params
+
+  private
+
+    def user_params
       params.require(:user).permit(:first_name, :last_name, :email, :password,
                                    :password_confirmation, :date_of_birth, :date_of_hire, :sex, :phone_number, :shirt_size, :suit_size)
     end
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+  end
 end
