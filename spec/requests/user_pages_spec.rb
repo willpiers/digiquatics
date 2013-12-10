d51ce3501f535cc86894f9d20cb453c2dae1d8aa
Joe Currin <joe@affektive.com>
2013-12-09 20:47:39 -0700

    fixed user spec page.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -59,5 +59,23 @@ describe "signup" do
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
+
+    describe "edit" do
+      let(:user) { FactoryGirl.create(:user) }
+      before { visit edit_user_path(user) }
+
+      describe "page" do
+        it { should have_content("Update your profile") }
+        it { should have_title("Edit user") }
+       
+      end
+
+      describe "with invalid information" do
+        before { click_button "Save changes" }
+
+        it { should have_content('error') }
+      end
+    end
   end
 end
+
