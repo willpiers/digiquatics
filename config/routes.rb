cd6ca2d0836b191febf7c8f2554158e245e73ea1
Michael Pierce <michael@affektive.com>
2013-12-24 16:19:50 -0700

    Fixed root to home

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ AquaticsApp::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
 
-  root  'certifications#index'
+  root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
