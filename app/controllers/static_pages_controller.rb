86bb053d4c7c70b5ee737ef56ca8d979e59c7d33
Michael Pierce <michael@affektive.com>
2013-12-24 20:13:33 -0700

    Dynamic headers for certs stats page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -19,6 +19,9 @@ class StaticPagesController < ApplicationController
   end
 
   def certifications_stats
+    @users = User.all
+    @certification_names = CertificationName.joins(:account).where(account_id: current_user.account_id)
+    @Certification_Count = Certification.joins(:user).where("expiration_date >=?", 30.days.ago.to_date).group(:certification_id)
   end
 
   def user_stats
