4f48b218cd5784bf45ecd2139f97ff3e43bf6191
Michael Pierce <michael@affektive.com>
2013-12-31 13:39:18 -0700

    Added age function to application helper and implemented on user index page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,6 +13,11 @@ module ApplicationHelper
     value ? "Yes" : "No"
   end
 
+  def age(dob)
+    now = Time.now.utc.to_date
+    now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+  end
+
   def sortable(column, title = nil)
     title ||= column.titleize
     direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
