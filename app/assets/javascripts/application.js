d42693a604fe08f51b937bcfc6988752687c214e
Chris Brady <cbrady@staticmotiondg.com>
2013-12-31 17:20:51 -0700

    Removed duplicate jQuery includes && unused pagination javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,10 +18,6 @@
 //= require bootstrap-datepicker
 
 $(function() {
-  $("#users th a, #users .pagination a").live("click", function() {
-    $.getScript(this.href);
-    return false;
-  });
   $("#users_search input").keyup(function() {
     $.get($("#users_search").attr("action"), $("#users_search").serialize(), null, "script");
     return false;
