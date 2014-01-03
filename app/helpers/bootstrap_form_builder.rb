a49fa44dc5575b6368a0b359ba44c52cf9819ddd
Michael Pierce <michael@affektive.com>
2014-01-02 19:36:09 -0700

    Same as below for helpers.

--- app/helpers/bootstrap_form_builder.rb
+++ app/helpers/bootstrap_form_builder.rb
@@ -8,13 +8,17 @@ class BootstrapFormBuilder < ActionView::Helpers::FormBuilder
   def standard_html type, method, options = {}
     options[:class] ||= ""
     options[:label] ||= method.to_s.titleize
-    field_errors = @object.errors[method].join(', ') if !@object.errors[method].blank?
+    field_errors = @object.errors[method].join(', ')
+      if !@object.errors[method].blank?
 
-    content_tag :div, class: "form-group #{ 'has-error' unless @object.errors[method].blank?}" do
-      content = label_tag("#{object_name}[#{method}]", "#{options[:label]}", :class => "control-label")
-      content << @template.send(type+'_tag', "#{object_name}[#{method}]", nil, :class => "form-control #{options[:class]}")
-      content << (content_tag(:span, field_errors.humanize, class: 'help-block')) if field_errors
-
+    content_tag :div, class: "form-group #{ 'has-error' 
+      unless @object.errors[method].blank?}" do
+      content = label_tag("#{object_name}[#{method}]", "#{options[:label]}", 
+        :class => "control-label")
+      content << @template.send(type+'_tag', "#{object_name}[#{method}]", nil, 
+        :class => "form-control #{options[:class]}")
+      content << (content_tag(:span, field_errors.humanize, 
+        class: 'help-block')) if field_errors
       content.html_safe
     end
   end
@@ -25,5 +29,4 @@ class BootstrapFormBuilder < ActionView::Helpers::FormBuilder
       standard_html(__method__.to_s, method, options)
     end
   end
-
 end
