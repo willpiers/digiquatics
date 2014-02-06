3aaf8c135b3566fdb4b1bee3438d1c57a4362f2e
Josh Duffy <jduffy@mines.edu>
2014-02-06 12:10:42 -0700

    server primary true

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@ set :stage, :production
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'digiquatics.com', user: 'deployer', roles: %w{web app}
+server 'digiquatics.com', user: 'deployer', roles: %w{web app db}, primary: true
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
