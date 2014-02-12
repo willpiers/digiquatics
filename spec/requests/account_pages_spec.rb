b8e6ac8b6e681d24dd4d3650cd06da2be3d4dcf3
Josh Duffy <jduffy@mines.edu>
2014-02-11 17:30:16 -0700

    removed user fields from account pages

--- spec/requests/account_pages_spec.rb
+++ spec/requests/account_pages_spec.rb
@@ -52,4 +52,15 @@ describe 'Account pages' do
       end
     end
   end
+
+  describe 'edit' do
+    let!(:user) { FactoryGirl.create(:user, account_id: account.id) }
+    before do
+      visit edit_account_path(account)
+    end
+
+    describe 'should not have user fields' do
+      it { should_not have_content('Basic User Information') }
+    end
+  end
 end
