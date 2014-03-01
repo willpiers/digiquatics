40c831896713bbd0ac7e1f4a3e078d2ca30fd3f5
Josh Duffy <jduffy@mines.edu>
2014-03-01 14:09:01 -0700

    fixed PR comments

--- app/helpers/private_lessons_helper.rb
+++ app/helpers/private_lessons_helper.rb
@@ -1,9 +1,5 @@
 module PrivateLessonsHelper
-  def student_full_name(student)
-    "#{student.first_name} #{student.last_name}"
-  end
-
-  def instructor_full_name(instructor)
-    "#{instructor.first_name} #{instructor.last_name}"
+  def full_name(person)
+    "#{person.first_name} #{person.last_name}"
   end
 end
