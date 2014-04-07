cb9d5019abf7ce5bb54f13cf1ebae138a914f5d4
Michael Pierce <michael@affektive.com>
2014-04-06 20:43:16 -0600

    Add phone number tel tags throughout app closes #428

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -84,4 +84,10 @@ module ApplicationHelper
         'Please use IE9+, Chrome, Firefox, Safari, or another modern browser.'
     end
   end
+
+  def phone_number_link(text)
+    sets_of_numbers = text.scan(/[0-9]+/)
+    number = "+1-#{sets_of_numbers.join('-')}"
+    link_to text, "tel:#{number}"
+    end
 end
