1b7efee3093204ea18d111a9d2ee9b3ecb16fbec
Michael Pierce <michael@affektive.com>
2014-04-11 07:53:31 -0600

    Fixed range on cert method

--- app/helpers/certifications_helper.rb
+++ app/helpers/certifications_helper.rb
@@ -34,11 +34,11 @@ module CertificationsHelper
 
   def css_class(date)
     case date
-      when (Date.today - 1000.days)..(Date.today)
+      when (Date.today - 3000.days)..(Date.today)
         'danger'
       when (Date.today)...(Date.today + 90.days)
         'warning'
-      when (Date.today + 90.days)...(Date.today + 180.days)
+      when (Date.today + 90.days)..(Date.today + 180.days)
         'cert_blue'
       else
         'success'
