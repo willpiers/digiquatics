1b42a9b06c73923a292d54f6ac864bd385e912a3
Josh Duffy <jduffy@mines.edu>
2014-05-09 15:58:37 -0600

    flash success

--- app/controllers/slide_inspections_controller.rb
+++ app/controllers/slide_inspections_controller.rb
@@ -20,11 +20,9 @@ class SlideInspectionsController < ApplicationController
   def create
     @slide_inspection = SlideInspection.new(slide_inspection_params)
     @slide_inspection.user_id = current_user.id
-
     @slide_inspection.all_ok = true if count_completed(slide_inspection_params) == 24
-
     if @slide_inspection.save
-      flash[:sucess] = 'Slide Inspection was successfully created.'
+      flash[:success] = 'Slide Inspection was successfully created.'
       redirect_to @slide_inspection
     else
       render action: 'new'
