e9a1c0d434e0d667458b86ae11889727fd0fce4b
duffcodester <jduffy@mines.edu>
2014-05-11 16:36:59 -0600

    Update help_desk_mailer.rb

--- app/mailers/help_desk_mailer.rb
+++ app/mailers/help_desk_mailer.rb
@@ -5,7 +5,7 @@ class HelpDeskMailer < ActionMailer::Base
     @recipients = User.where(admin: true, account_id: account_id)
     emails = @recipients.collect(&:email).join(",")
     @help_desk = issue
-    mail(to:  emails, from: "team@digiquatics.com",
+    mail(to: emails, from: "team@digiquatics.com",
          subject: "Urgent Help Desk Issue at #{Location.find(@help_desk.location_id).name}")
   end
 end
