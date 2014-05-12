0b3741530a73f4f8d894c8a591012b4283dbc64f
Michael Pierce <michael@affektive.com>
2014-05-11 18:08:58 -0600

    Help desk true false to open closes closes #577

--- app/helpers/help_desks_helper.rb
+++ app/helpers/help_desks_helper.rb
@@ -2,4 +2,8 @@ module HelpDesksHelper
   def approve_link_text(approvable)
     approvable.approved? ? 'Un-approve' : 'Approve'
   end
+
+  def boolean_to_words(value)
+    value ? 'Open' : 'Closed'
+  end
 end
