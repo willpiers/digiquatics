a87a7164ffffc37ec697536af60302b38e7da9f8
Kelton Manzanares <kmanzana@mines.edu>
2014-05-24 11:04:09 -0600

    so fresh and so clean, clean

--- app/facades/static_pages_facade.rb
+++ app/facades/static_pages_facade.rb
@@ -7,24 +7,23 @@ class StaticPagesFacade
   end
 
   def unclaimed_for(location)
-    @private_lessons.where(location_id: location.id, user_id: nil).count
+    @private_lessons.for_location(location).unclaimed.count
   end
 
   def claimed_for(location)
-    @private_lessons.where(location_id: location.id).where.not(user_id: nil)
-    .count
+    @private_lessons.for_location(location).claimed.count
   end
 
   def total_for(location)
-    @private_lessons.where(location_id: location.id).count
+    @private_lessons.for_location(location).count
   end
 
   def unclaimed_total
-    @unclaimed_total ||= @private_lessons.where(user_id: nil).count
+    @unclaimed_total ||= @private_lessons.unclaimed.count
   end
 
   def claimed_total
-    @claimed_total ||= @private_lessons.where.not(user_id: nil).count
+    @claimed_total ||= @private_lessons.claimed.count
   end
 
   def total
