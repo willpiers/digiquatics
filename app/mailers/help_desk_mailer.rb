0627ff3cc781b023b1f7c1fbb2b69d270cb3ebb0
Michael Pierce <michael@affektive.com>
2014-05-24 11:52:24 -0600

    Fix on location bug for emails.

--- app/mailers/help_desk_mailer.rb
+++ app/mailers/help_desk_mailer.rb
@@ -2,7 +2,8 @@ class HelpDeskMailer < ActionMailer::Base
   default from: 'Team@digiquatics.com'
 
   def urgent_email(issue, account_id)
-    @recipients = User.where(admin: true, account_id: account_id).where.not(email: nil)
+    @recipients = User.where(admin: true, account_id: account_id)
+                      .where.not(email: nil, location_id: nil)
     emails = @recipients.map(&:email).join(',')
     @help_desk = issue
     mail(to: emails, from: 'Team@digiquatics.com',
