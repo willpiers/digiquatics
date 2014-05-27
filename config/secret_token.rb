768c871849c26213a445a95a50d87fb89dd7de8a
Josh Duffy <jduffy@mines.edu>
2014-05-27 08:52:25 -0600

    added secret_token file back in

---
+++ config/secret_token.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+DigiQuatics::Application.config.secret_key_base = '3a1e521c7c894fc612db4ea16' \
+  '0fff7da3d968e56ea95f6ee8dc47dadaed614fb670d56108f20718c67aabc32f83d5a5d1c' \
+  '1de5a0c3cf210f9f2c973613fa90de'
