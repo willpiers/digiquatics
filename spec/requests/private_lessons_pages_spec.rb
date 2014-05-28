43bfe38fab199bf60864623362c5e8e3845adf73
Michael Pierce <michael@affektive.com>
2014-05-28 09:50:34 -0600

    Tests for lesson arrangement time

--- spec/requests/private_lessons_pages_spec.rb
+++ spec/requests/private_lessons_pages_spec.rb
@@ -276,6 +276,9 @@ describe 'Private Lessons' do
         let(:student_first_name) { 'Johnny' }
 
         before do
+          # Meeting Arrangement
+          fill_in 'private_lesson_meeting_time_agreement', with: 'Sat 8am'
+
           # Parent information
           fill_in 'Parent First Name', with: 'Parent First'
           fill_in 'Parent Last Name',  with: 'Parent Last'
@@ -345,7 +348,6 @@ describe 'Private Lessons' do
       end
     end
   end
-
 
   describe 'stats' do
     before do
