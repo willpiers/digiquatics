a5a0961adb6cbd9b7885a9d6706c5b078c80b51f
Michael Pierce <michael@affektive.com>
2014-05-31 10:33:13 -0600

    Test for instructions page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'Static pages' do
-  describe 'Home page' do
+  describe 'Landing page' do
     before { visit '/' }
 
     it 'should have the content \'DigiQuatics\'' do
@@ -15,5 +15,28 @@ describe 'Static pages' do
     it 'should not have a custom page title' do
       expect(page).not_to have_title('| Home')
     end
+  end
+
+  describe 'Instructions page' do
+    let!(:account) { FactoryGirl.create(:account) }
+    let(:user) do
+      FactoryGirl.create(:admin,
+                         account_id: account.id )
+    end
+
+    before do
+      login_as(user, scope: :user)
+    end
+
+    before { visit '/instructions' }
+
+    it 'should have the content \'Instructions\'' do
+      expect(page).to have_content('Instructions')
+    end
+
+    it 'should have the base title' do
+      expect(page).to have_title('Instructions')
+    end
+
   end
 end
