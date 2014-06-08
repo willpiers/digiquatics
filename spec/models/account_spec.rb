c4afa8e43633f820037b31fe6d2cbd98b00451c2
Michael Pierce <michael@affektive.com>
2014-06-08 13:00:05 -0600

    Added tests for accounts model.

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -3,7 +3,16 @@ require 'spec_helper'
 describe Account do
   before do
     @account = Account.new(name: 'City of Lakewood',
-                           time_zone: 'Mountain Time (US & Canada)')
+                           time_zone: 'Mountain Time (US & Canada)',
+                           slides_group_email:              false,
+                           slides_admin_email:              false,
+                           slides_location_email:           false,
+                           maintenance_group_email:         false,
+                           maintenance_admin_email:         false,
+                           maintenance_location_email:      false,
+                           chemical_records_group_email:    false,
+                           chemical_records_admin_email:    false,
+                           chemical_records_location_email: false)
   end
 
   subject { @account }
@@ -11,6 +20,15 @@ describe Account do
   it { should respond_to :name }
   it { should respond_to :time_zone }
   it { should respond_to(:logo) }
+  it { should respond_to(:slides_group_email) }
+  it { should respond_to(:slides_admin_email) }
+  it { should respond_to(:slides_location_email) }
+  it { should respond_to(:maintenance_group_email) }
+  it { should respond_to(:maintenance_admin_email) }
+  it { should respond_to(:maintenance_location_email) }
+  it { should respond_to(:chemical_records_group_email) }
+  it { should respond_to(:chemical_records_admin_email) }
+  it { should respond_to(:chemical_records_location_email) }
 
   it { should be_valid }
 
